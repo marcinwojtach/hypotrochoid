2e17f52e269a103f6494cab2fa5348aba4aa1885
Marcin Wojtach <marcin.vojtach@gmail.com>
2024-06-06 14:54:45 +0200

    add gui labels

--- src/gui.ts
+++ src/gui.ts
@@ -5,17 +5,21 @@ export default (defaults: Params, drawFn: (options: Params) => void) => {
   const gui: dat.GUI = new dat.GUI({ width: 300 });
 
   gui.add(defaults, 'fixedCircleRadius', 50, 250)
-   .step(1)
-   .onChange((fixedCircleRadius) => drawFn({...defaults, fixedCircleRadius }))
+    .name('Fixed circle radius')
+    .step(1)
+    .onChange((fixedCircleRadius) => drawFn({ ...defaults, fixedCircleRadius }))
 
   gui.add(defaults, 'rollingCircleRadius', 50, 250)
-   .step(1)
-   .onChange((rollingCircleRadius) => drawFn({...defaults, rollingCircleRadius }))
+    .name('Rolling circle radius')
+    .step(1)
+    .onChange((rollingCircleRadius) => drawFn({ ...defaults, rollingCircleRadius }))
 
   gui.add(defaults, 'distanceToTracingPoint', 50, 500)
-   .step(1)
-   .onChange((distanceToTracingPoint) => drawFn({...defaults, distanceToTracingPoint }))
+    .name('Distance to tracing point')
+    .step(1)
+    .onChange((distanceToTracingPoint) => drawFn({ ...defaults, distanceToTracingPoint }))
 
   gui.addColor(defaults, 'strokeColor')
-    .onChange((strokeColor) => drawFn({...defaults, strokeColor }))
+    .name('Stroke color')
+    .onChange((strokeColor) => drawFn({ ...defaults, strokeColor }))
 };
